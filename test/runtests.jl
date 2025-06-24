d3a493a720ad432eafc3bc44ed93cbf883d2e1fd
Moelf <proton@jling.dev>
2025-06-24 14:37:04 -0400

    fixing tests and use Aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,5 @@
-using BinnedDistributionFit
-using Test, FHist
-using TestItems
+using TestItems, TestItemRunner
+@run_package_tests
 
 @testitem "ExtendPdf" begin
     d = ExtendPdf((x, _)->x, (1,3))
@@ -25,6 +24,7 @@ using TestItems
     end
 
 @testitem "RooFitNLL ExtendPdf" begin
+    using FHist
     d = ExtendPdf((x, _)->x, (1,3))
     h = Hist1D(; binedges=1:3, bincounts=[2.0, 4.0], sumw2=[2.0, 4.0])
     NF = RooFitNLL_functor(d, h; num_integrator=BinnedDistributionFit.QuadGKIntegrator())
@@ -32,11 +32,18 @@ using TestItems
 end
 
 
-@testset "RooFit SumOfPdfs" begin
+@testitem "RooFit SumOfPdfs" begin
+    using FHist
     d1 = ExtendPdf((x, _)->x, (1,3))
     d2 = ExtendPdf((x, _)->x^2, (1,3))
     sd = d1+d2
     h = Hist1D(; binedges=1:3, bincounts=[2.0, 4.0], sumw2=[2.0, 4.0])
     NF = RooFitNLL_functor(sd, h; num_integrator=BinnedDistributionFit.QuadGKIntegrator())
     @test NF([[2.0, 0.5], [], []]) ≈ 0.8497340452248006
+end
+
+
+@testitem "_" begin
+    using Aqua
+    Aqua.test_all(BinnedDistributionFit)
 end
