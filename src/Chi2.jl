e13323434775b11b2a24e599672bc1f2fdd28e1f
Alexi-M <aleximeyers@gmail.com>
2025-06-27 15:52:34 -0400

    changed chi2_functor binerrors syntax (from comment)

--- src/Chi2.jl
+++ src/Chi2.jl
@@ -1,7 +1,7 @@
 using FHist
 
 function Chi2_functor(d::ExtendPdf, data_hist::Hist1D; num_integrator = SimpleSumIntegrator())
-    bes, bcs = binedges(data_hist), bincenters(data_hist)
+    bes, bcs, ber = binedges(data_hist), bincenters(data_hist), binerrors(data_hist)
     obs = bincounts(data_hist)
     @assert extrema(bes) == extrema(d.support) "The support of the distribution must match the bin edges of the histogram."
     
@@ -10,7 +10,7 @@ function Chi2_functor(d::ExtendPdf, data_hist::Hist1D; num_integrator = SimpleSu
         expected = vector_eval(d, bcs, ps; kw...)
         oneD_func(x) = scalar_eval(d, x, ps; kw...)
         normedExp =  expected ./ _integrate(oneD_func, data_hist, num_integrator)
-        return chi2(obs, norm * normedExp, binerrors(data_hist))
+        return chi2(obs, norm * normedExp, ber)
     end
 end
 
@@ -19,7 +19,7 @@ function chi2(obs, expected, sd)
 end
 
 function Chi2_functor(d::SumOfPdfs, data_hist::Hist1D; num_integrator = SimpleSumIntegrator())
-    bes, bcs = binedges(data_hist), bincenters(data_hist)
+    bes, bcs, ber = binedges(data_hist), bincenters(data_hist), binerrors(data_hist)
     obs = bincounts(data_hist)
     @assert extrema(bes) == extrema(d.support) "The support of the distribution must match the bin edges of the histogram."
     function (norms_and_vps; kw...)
@@ -37,6 +37,6 @@ function Chi2_functor(d::SumOfPdfs, data_hist::Hist1D; num_integrator = SimpleSu
         end
         overall_norms = sum(norms)
         normed_predictions = sum(abs2.(norms) / overall_norms .* predictions_of_pdfs ./ integrals_of_pdfs)
-        return chi2(obs, normed_predictions, binerrors(data_hist))
+        return chi2(obs, normed_predictions, ber)
     end
 end
