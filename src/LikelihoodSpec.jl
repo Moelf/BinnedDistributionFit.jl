c51a77253bfa5609957cb563d86111f7f8c80d3b
Alexi-M <aleximeyers@gmail.com>
2025-07-10 12:51:59 -0400

    fixed code duplication issue

--- src/LikelihoodSpec.jl
+++ src/LikelihoodSpec.jl
@@ -189,29 +189,3 @@ function (NLL2::LikelihoodSpec{<:CSQ})(nps::ComponentVector; kw...)
     # return sum(@. abs2((NLL2.d_hist.bincounts-normed_predictions)/ber))
 end
 
-function (NLL2::LikelihoodSpec{<:CSQ})(nps::ComponentVector; kw...)
-    ber = binerrors(NLL2.d_hist)
-    vks = valkeys(nps)
-    norms = nps[vks[begin]]
-    pdfs = get_pdf(NLL2.pdf)
-    Npdfs = length(pdfs)
-    if length(norms) != length(pdfs)
-        throw(ArgumentError("Expected $Npdfs normalizations, got $(length(norms))"))
-    end
-    if length(vks)-1 != Npdfs
-        throw(ArgumentError("Expected $Npdfs set$((Npdfs > 1) ? "s" : "") of parameters, got $(length(vks)-1)"))
-    end
-    integrals_of_pdfs = map(pdfs, vks[(begin+1):end]) do d, ps
-        p0 = getproperty(nps, ps)
-        oneD_func(x) = scalar_eval(d, x, p0; kw...)
-        _integrate(oneD_func, NLL2.d_hist, NLL2.num_int; kw...)
-    end
-    predictions_of_pdfs = map(pdfs, vks[(begin+1):end]) do d, ps
-        p0 = getproperty(nps, ps)
-        vector_eval(d, NLL2.bcs, p0, kw...)
-    end
-    overall_norms = sum(norms)
-    normed_predictions = sum(abs2.(norms) / overall_norms .* predictions_of_pdfs ./ integrals_of_pdfs)
-    return BinnedDistributionFit.chi2(NLL2.d_hist.bincounts, normed_predictions, ber)
-    # return sum(@. abs2((NLL2.d_hist.bincounts-normed_predictions)/ber))
-end
